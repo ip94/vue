eec56bbe8c3d14d4da50f1b15ad8c1d871844f4a
ip94 <irene830926@gmail.com>
2017-11-14 03:02:14 +0000

    Fix import path

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-    import Station from "/components/Station.vue"
+    import Station from "./components/Station.vue"
     export default {
       name: 'app',
       components: {
