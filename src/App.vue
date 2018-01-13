e4037bdca71e38c90c136b0e01937efd0b7e1623
ip94 <ip94@users.noreply.github.com>
2018-01-13 18:06:25 -0500

    level up

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
 <template>
-<div id="app">
-  <div class="container">
-    <div class="jumbotron">
-        <h1>Bike(whe)re<br><small>Can you get there?</small></h1>
-        <h3>An app to help you navigate through the Toronto Bikeshare system where you need it, when you need it.</h3>
-    </div>
-    <ul class="nav nav-tabs">
-      <li class="nav-item nav-fill">
-        <a class="nav-link" href="/intro.html">Introduction</a>
-      </li>
-      <li class="nav-item nav-fill">
-        <a class="nav-link" href="/install2.html">Installation</a>
-      </li>
-      <li class="nav-item nav-fill">
-        <a class="nav-link" href="/howto3.html">Tutorial</a>
-      </li>
-      <li class="nav-item nav-fill">
-        <a class="nav-link active" href="#">Bike(whe)re</a>
-      </li>
-      <li class="nav-item nav-fill">
-        <a class="nav-link" href="/conclusion5.html">Conclusion</a>
-      </li>
-      <li class="nav-item nav-fill">
-        <a class="nav-link" href="/reference6.html">References</a>
-      </li>
-    </ul>
+<div class="container"id="app">
+  <div class="jumbotron">
+      <h1>Bike(whe)re<br><small>Can you get there?</small></h1>
+      <h3>An app to help you navigate through the Toronto Bikeshare system where you need it, when you need it.</h3>
   </div>
-  <div class="container">
-    <Station v-on:update="updateMessage"/>
-    <div class="container center-block panel">
-        <div class="text-left text-info col-lg-12 panel-heading">
-            <h2 style="">
-                {{message}}
-            </h2>
-        </div>
-    </div>
+  <ul class="nav nav-tabs">
+    <li class="nav-item nav-fill">
+      <a class="nav-link" href="/intro.html">Introduction</a>
+    </li>
+    <li class="nav-item nav-fill">
+      <a class="nav-link" href="/install2.html">Installation</a>
+    </li>
+    <li class="nav-item nav-fill">
+      <a class="nav-link" href="/howto3.html">Tutorial</a>
+    </li>
+    <li class="nav-item nav-fill">
+      <a class="nav-link active" href="#">Bike(whe)re</a>
+    </li>
+    <li class="nav-item nav-fill">
+      <a class="nav-link" href="/conclusion5.html">Conclusion</a>
+    </li>
+    <li class="nav-item nav-fill">
+      <a class="nav-link" href="/reference6.html">References</a>
+    </li>
+  </ul>
+  <Station v-on:update="updateMessage"/>
+  <div class="container center-block panel">
+      <div class="text-left text-info col-lg-12 panel-heading">
+          <h2 style="">
+              {{message}}
+          </h2>
+      </div>
   </div>
 </div>
 </template>
